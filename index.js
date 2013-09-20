fe4e9e36e5f890849d57283327faf3c07c69617e
blacksea <ag@theblacksea.cc>
2013-09-19 19:20:20 -0600

    ignore buffers

--- index.js
+++ index.js
@@ -6,25 +6,23 @@ inherits(Fern,Stream)
 module.exports = Fern
 
 function Fern (opts) {
-  if (!opts.key && !opts.tree) console.error('Fern needs an options object in the format: {key:String,tree:Object}')
+  if (!opts.key && !opts.tree) throw new Error('Fern needs an options object in the format: {key:String,tree:Object}')
   Stream.call(this) 
   this.readable = true
   this.writable = true
   var self = this  
   
   this.write = function (chunk) {
-    if (typeof chunk === 'string') var d = JSON.parse(chunk)
-    if (typeof chunk !== 'string') console.error('not a STRING')
+    if (!(chunk instanceof Buffer)) var d = JSON.parse(chunk) // todo: ignore if buffer
     if (d instanceof Object && d[opts.key]) {
       var a = d[opts.key]
       var cmd = a[0]
       var param = a[1]
       var cb = a[2]
       opts.tree[cmd](param, function handleResult (val) {
-        self.emit('data',JSON.stringify({res:val,fn: cmd}))
+        self.emit('data',JSON.stringify({res:val}))
       })
-    } else {
-      console.error('no match!')
+    } else { // ignore & pass through 
       self.emit('data',chunk)
     } 
   } 
