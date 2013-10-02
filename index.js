5d89ea19e889da6d650bb66da8bfb7c92ebe0948
nimoy <ag@theblacksea.cc>
2013-10-01 22:14:06 -0600

    switched to through instead of inheriting from Stream

--- index.js
+++ index.js
@@ -1,19 +1,13 @@
-var Stream = require('stream').Stream
-var inherits = require('inherits')
-
-inherits(Fern,Stream)
+var through = require('through')
 
 module.exports = Fern
 
 function Fern (opts) {
   if (!opts && !opts.tree) throw new Error('Fern needs an options object in the format: {key:String,tree:Object}')
   // make opts.key optional, if not specified just try to match tree.method
-  Stream.call(this) 
-  this.readable = true
-  this.writable = true
-  var self = this  
-  
-  this.write = function (chunk) {
+
+  var s = through(function write (chunk) {
+    var self = this
     var d = JSON.parse(chunk) // todo: ignore if buffer
     if (d instanceof Object && d[opts.key]) {
       var a = d[opts.key]
@@ -30,6 +24,7 @@ function Fern (opts) {
     if (d instanceof Array && !d[opts.key]) {
       var cmd = d[0]
       var param = d[1]
+      if (!opts.tree[cmd]) console.error('no such command!')
       if (opts.tree[cmd]) {
         opts.tree[cmd](param, function handleResult (val) {
           self.emit('data',JSON.stringify({res:val}))
@@ -39,6 +34,9 @@ function Fern (opts) {
     if (!d[opts.key] && !(d instanceof Array)) {
       self.emit('data', chunk)
     }
-  }
-  this.end = function () {} 
-} 
+  }, function end () {
+    this.emit('end')
+  })
+
+  return s
+}:
