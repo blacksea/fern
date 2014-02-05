77d03c56d1f5c6399476d292c5cb3eb0726e9c68
nimoy <ag@theblacksea.cc>
2014-02-05 02:02:13 -0700

    no need to handle put/del differently

--- index.js
+++ index.js
@@ -9,15 +9,9 @@ module.exports = function Fern (tree) {
     if (d.key && d.value && d.type) {
       var path = d.key.split(':')
       var fn = path[0] // check fn for callback
-      if (d.type === 'put') { 
-        tree[d.type][fn](d, function (res) {
-          s.emit('data', res)
-        })
-      } else {
-        tree[d.type](d, function (res) {
-          s.emit('data', res)
-        })
-      }
+      tree[d.type][fn](d, function (res) {
+        s.emit('data', res)
+      })
     } else 
       this.emit('error', new Error('input should be leveldb livestream obj like: {key: string,value: json, type:string}'));
   }, function end () {
