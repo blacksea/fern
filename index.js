3f5b9da53db92b7101b70d06064223882128db27
nimoy <ag@theblacksea.cc>
2014-02-06 00:15:01 -0700

    log failed obj input

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ var through = require('through')
 
 module.exports = function Fern (tree) {
   // make sure tree is valid
-  if (typeof tree !== 'object' && !tree.put && !tree.del) throw new Error('please pass a fn tree with put & del properties')
+  if (typeof tree !== 'object' && !tree.put && !tree.del) 
+    throw new Error('please pass a fn tree with put & del properties')
 
   var s = through(function handleData (d) {// should be incoming leveldb livestream
 
@@ -14,7 +15,7 @@ module.exports = function Fern (tree) {
         if (!e) s.emit('data', res)
       })
     } else 
-      this.emit('error', new Error('input should be leveldb livestream obj like: {key: string,value: json, type:string}'));
+      this.emit('error', new Error(d + ' : input should be leveldb livestream obj like: {key: string,value: json, type:string}'));
   }, function end () {
     this.emit('end')
   }, {autoDestroy:false})
